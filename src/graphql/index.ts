1cb24da715ec36bb0bd3a152b798784cf15443fc
= <=>
2023-01-20 17:24:19 +0800

    changed url

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-    uri: 'http://localhost:8080/graphql',
+    uri: 'http://birmanila.nat911.com/api/v2/graphql',
     cache: new InMemoryCache(),
 });
 
